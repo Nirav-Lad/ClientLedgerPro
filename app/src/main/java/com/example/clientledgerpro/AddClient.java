8544b6a52431738824dc79b0ab868dba3afea150
Nirav-Lad <nirav20267@gmail.com>
2024-09-23 23:01:42 +0530

    Add client, transactions and project activity added/

--- app/src/main/java/com/example/clientledgerpro/AddClient.java
+++ app/src/main/java/com/example/clientledgerpro/AddClient.java
@@ -14,6 +14,7 @@ public class AddClient extends AppCompatActivity {
 
     private Button addClient;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/clientledgerpro/Transaction.java
+++ app/src/main/java/com/example/clientledgerpro/Transaction.java
@@ -15,17 +15,16 @@ public class Transaction extends AppCompatActivity {
 
     private Button addTransaction;
     private Button canceltransac;
+    private Button project;
+    private Button client;
+    private Button transaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_transaction);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
 
         addTransaction=findViewById(R.id.add_transaction_button);
         addTransaction.setOnClickListener(new View.OnClickListener() {
@@ -33,6 +32,34 @@ public class Transaction extends AppCompatActivity {
             public void onClick(View view) {
                 Intent addtrans=new Intent(Transaction.this, AddTransaction.class);
                 startActivity(addtrans);
+            }
+        });
+
+        project=findViewById(R.id.button_projects);
+        client=findViewById(R.id.button_clients);
+        transaction=findViewById(R.id.button_transactions);
+
+        project.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent project=new Intent(Transaction.this, activity_project.class);
+                startActivity(project);
+            }
+        });
+
+        client.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent client=new Intent(Transaction.this, activity_client.class);
+                startActivity(client);
+            }
+        });
+
+        transaction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent transaction=new Intent(Transaction.this, Transaction.class);
+//                startActivity(transaction);
             }
         });
     }
