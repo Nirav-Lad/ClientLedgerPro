1b3915d80d359f097731bbb3c1f7a49b20cfde79
Yuvraj Ahuja <yuvraj.ahuja@parthvi.tech>
2024-09-24 08:13:37 +0530

    activity_main was initially open on creation is now changed directly client_dashboard can be opened. Changes made in the AndroidManifest file and Some in MainActivity.java file.

--- app/src/main/java/com/example/clientledgerpro/MainActivity.java
+++ app/src/main/java/com/example/clientledgerpro/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.clientledgerpro;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -28,6 +29,13 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = new Intent(this, client_dashboard.class);
         startActivity(intent);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
 
     }
 }
